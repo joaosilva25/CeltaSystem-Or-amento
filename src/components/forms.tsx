2f72d627660c2438eab461ed13808b5e7bffcff1
JoaoSilva25 <joaosilva2505@outlook.com>
2025-03-12 12:45:20 -0300

    testando

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -191,6 +191,7 @@ export default function FormTemplate() {
           <label className="mt-10 block text-sm/6 font-bold text-gray-900">
             Tipo de Transação*
           </label>
+          {/* teste */}
           <select
             className="mb-4 h-12 block w-full bg-transparent px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2  sm:text-sm/6"
             value={tipoTransacao}
